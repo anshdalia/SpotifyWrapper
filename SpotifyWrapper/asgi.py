9ca815a7fa9129f5a529ce828998a72b65d326aa
nvphelps <nvphelps815@gmail.com>
2024-10-27 09:23:12 -0400

    Added Registration page functionality

--- SpotifyWrapper/asgi.py
+++ SpotifyWrapper/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for spotifyWrapper project.
+ASGI config for SpotifyWrapper project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'spotifyWrapper.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'SpotifyWrapper.settings')
 
 application = get_asgi_application()
--- user/views.py
+++ user/views.py
@@ -1,6 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from . forms import createUserForm
 
 # Create your views here.
 
-def welcome(request):
-    return render(request, 'user/welcome.html')
+
+def register(request):
+
+    form = createUserForm()
+
+    if request.method == 'POST':
+        form = createUserForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+
+    context = {'registerForm':form}
+
+    return render(request, 'register.html', context)
+
+
+
