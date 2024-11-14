86afb022606d5f10294fe013da1e5b89b9ac961b
krith-raju <kraju30@gatech.edu>
2024-11-13 21:46:41 -0500

    Upated ALLOWED_HOSTS for Heroku

--- SpotifyWrapper/settings.py
+++ SpotifyWrapper/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG') == 'True'
 
-ALLOWED_HOSTS = ['*']  # Allow all hosts in production; you may specify domains here
+ALLOWED_HOSTS = ['spotify-wrapper-group35']  # Allow all hosts in production; you may specify domains here
 
 
 # Application definition
